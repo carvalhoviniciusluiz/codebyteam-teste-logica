e078fdc65da28b5e7f93af9e8c92f8f80f97f7e9
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2020-07-04 12:49:32 -0300

    adding solution

--- productStructure.js
+++ productStructure.js
@@ -1,18 +1,37 @@
 const products = [
-  "preto-PP", 
-  "preto-M", 
-  "preto-G", 
-  "preto-GG", 
-  "preto-GG", 
-  "branco-PP", 
-  "branco-G", 
-  "vermelho-M", 
-  "azul-XG", 
-  "azul-XG", 
-  "azul-XG", 
+  "preto-PP",
+  "preto-M",
+  "preto-G",
+  "preto-GG",
+  "preto-GG",
+  "branco-PP",
+  "branco-G",
+  "vermelho-M",
+  "azul-XG",
+  "azul-XG",
+  "azul-XG",
   "azul-P"
 ]
 
+const parseProducts = (products = []) => {
+  return products.reduce((acc, cur) => {
+    const [key, value] = cur.split('-');
+    const hasKey = Object.keys(acc).includes(key);
+
+    if (!hasKey) {
+      acc[key] = {};
+    }
+
+    const hasValue =  Object.keys(acc[key]).includes(value);
+
+    acc[key][value] = !hasValue
+      ? 1
+      : ++acc[key][value];
+
+    return acc;
+  }, {})
+}
+
 module.exports = () => {
-  // Código aqui
+  return parseProducts(products);
 }
